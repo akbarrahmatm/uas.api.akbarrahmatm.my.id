e96a2456f0fdbdc527c1c77bfa7ce53342b943ce
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2023-10-11 08:57:32 +0700

    up db conf

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'akbarra1_admin',
+        'password' => 'GuG=hE1Y4vUd',
+        'database' => 'akbarra1_cari_kelontong',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
